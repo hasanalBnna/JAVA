ab4675c9a6f87f3035b8bb9996ea91b0541bfc55
Kazi Hasan Al Banna <kazi.hasanalbanna59@gmail.com>
2024-04-29 11:11:10 +0600

    8th commit

---
+++ OOP/src/Encapsulation/Main.java
@@ -0,0 +1,18 @@
+
+package Encapsulation;
+
+
+public class Main {
+
+   
+    public static void main(String[] args) {
+        
+        person p1 =new person();
+        p1.setName("banna");
+        p1.setAge(27);
+        System.out.println("Name: "+p1.getName());
+        System.out.println("Age: "+p1.getAge());
+    
+    }
+    
+}
