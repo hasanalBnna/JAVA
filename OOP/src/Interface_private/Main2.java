f70c2936508843d733379a98d4995d7cbaf67e77
Kazi Hasan Al Banna <kazi.hasanalbanna59@gmail.com>
2024-05-12 23:59:49 +0600

    2nd commit

--- OOP/src/Interface_private/Main2.java
+++ OOP/src/Interface_private/Main2.java
@@ -2,10 +2,10 @@
 package Interface_private;
 
 interface A{
-    private void sum(int x, int y){
+    private static void sum(int x, int y){
         System.out.println("sum of this: "+(x+y));
     }
-    default void call(){
+    public static void call(){
         sum(20,10);
     }
 }
@@ -14,11 +14,11 @@ class B implements A{
         System.out.println("sub of this: "+(x-y));
     }
 }
-public class Main {
+public class Main2 {
 
     public static void main(String[] args) {
         B b = new B();
-        b.call();
+        A.call();
         b.sub(200, 100);
     }
     
